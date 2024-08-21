20c8639630218b57242732df9231f0294e47a81f
Daniel Cueto <dcueto14.info@gmail.com>
2024-08-21 13:10:42 +0200

    Hydrate template content at loading page into mini-island custom element by default (no attribute to set type of island hydration)

--- mini-island.js
+++ mini-island.js
@@ -7,16 +7,44 @@ class MiniIsland extends HTMLElement {
   static attributes = {
     dataIsland: 'data-island',
   };
+
+  // custom element method that is invoked when our custom element is attached to the DOM
+  async connectedCallback() {
+    await this.hydrate();
+  }
+
+  hydrate() {
+    const relevantChildTemplates = this.getTemplates();
+
+    console.log( relevantChildTemplates );
+
+    if ( relevantChildTemplates instanceof NodeList && relevantChildTemplates.length > 0 ){
+      this.replaceTemplates(relevantChildTemplates);
+      return;
+    }
+
+    console.log("There's no template tags into your mini-island element. Implement a <template> tag for rendering hydrated content");
+  }
+
+  getTemplates() {
+    return this.querySelectorAll(`template[${MiniIsland.attributes.dataIsland}]`);
+  }
+
+  // iterates each template and replaces the <template> tag with its inner HTML content
+  replaceTemplates(templates) {
+    for (const node of templates) {
+      // console.log('Node from templates', node);
+      // console.log('Node template content', node.content);
+      node.replaceWith(node.content);
+    }
+  }
 }
 
 // checks if browser supports native custom elements
 if ('customElements' in window) {
-
-  // registers our custom element 
+  // registers our custom element
   window.customElements.define(MiniIsland.tagName, MiniIsland);
 
 } else {
-
   console.error('Island cannot be initiated because Window.customElements is unavailable in your browser.');
-
 };
